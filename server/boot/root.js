0a926b6891c3feabba5cc92a1b7b8762cdaca201
Will Madden <w.a.madden@gmail.com>
2014-11-29 17:28:31 +0100

    Made the server serve the client/dist/index file on the root URL

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,9 @@
+var path = require('path');
+var config = require('config');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', server.loopback.static(path.resolve(__dirname, config.fileToServeOnRoot)));
   server.use(router);
 };
